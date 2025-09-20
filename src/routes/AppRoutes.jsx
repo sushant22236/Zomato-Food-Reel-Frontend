443c1089ffbf5b1262e8c3044619d002dd5aefe0
Sushant Singh <sushant22007@gmail.com>
2025-09-20 10:29:51 +0530

    Add ChooseRegister component and update routing for user and food partner registration and login

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserRegister from '../pages/auth/UserRegister';
+import UserLogin from '../pages/auth/UserLogin';
+import FoodPartnerRegister from '../pages/auth/FoodPartnerRegister';
+import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/user/register" element={<UserRegister />} />
+        <Route path="/user/login" element={<UserLogin />} />
+        <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
+        <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
       </Routes>
     </Router>
   );
